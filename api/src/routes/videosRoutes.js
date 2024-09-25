3238e143a2a497ae9b877acef9a03fcc1f798d4c
morian123 <morianlares@gmail.com>
2024-09-25 15:56:10 -0300

    PEGA FI

--- api/src/routes/videosRoutes.js
+++ api/src/routes/videosRoutes.js
@@ -1,104 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const dados = require("../mock/dados.json");
 
-// Rota para listar todos os vídeos
-router.get("/", (req, res) => {
-  try {
-    const videos = dados.videos;
+const videosControllers = require("../controllers/videosControllers");
 
-    if (videos.length > 0) {
-      res.status(200).json(videos);
-    } else {
-      res.status(404).json({ mensagem: "Nenhum vídeo encontrado" });
-    }
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro ao buscar vídeos", detalhes: erro.message });
-  }
-});
+router.get("/", videosControllers.index);
 
-// Rota para buscar um vídeo pelo ID
-router.get("/:id", (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const video = dados.videos.find((v) => v.id === id);
+router.get("/:id",videosControllers.show);
 
-    if (video) {
-      res.status(200).json(video);
-    } else {
-      res.status(404).json({ mensagem: "Vídeo não encontrado" });
-    }
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro ao buscar vídeo", detalhes: erro.message });
-  }
-});
+router.post("/", videosControllers.store);
 
-// Rota para criar um novo vídeo
-router.post("/", (req, res) => {
-  try {
-    const { titulo, descricao, quantidadeViews, image, canalID } = req.body;
+router.put("/:id", videosControllers.update);
 
-    const gerarIdUnico = () => {
-      let id;
-      do {
-        id = Math.floor(Math.random() * 1000);
-      } while (dados.videos.some((video) => video.id === id));
-      return id;
-    };
-
-    const novoVideo = {
-      id: gerarIdUnico(),
-      titulo,
-      descricao,
-      quantidadeViews,
-      image,
-      canalID
-    };
-
-    dados.videos.push(novoVideo);
-    res.status(201).json(novoVideo);
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro ao criar vídeo", detalhes: erro.message });
-  }
-});
-
-// Rota para editar um vídeo pelo ID
-router.put("/:id", (req, res) => {
-  try {
-    const { titulo, descricao, quantidadeViews, canalID } = req.body;
-    const id = parseInt(req.params.id);
-    const video = dados.videos.find((video) => video.id === id);
-
-    if (!video) {
-      return res.status(404).json({ mensagem: "Vídeo não encontrado" });
-    }
-
-    video.titulo = titulo;
-    video.descricao = descricao;
-    video.quantidadeViews = quantidadeViews;
-    video.canalID = canalID;
-
-    res.status(200).json(video);
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro ao editar vídeo", detalhes: erro.message });
-  }
-});
-
-// Rota para deletar um vídeo pelo ID
-router.delete("/:id", (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const indiceDoVideo = dados.videos.findIndex((video) => video.id === id);
-
-    if (indiceDoVideo !== -1) {
-      const videoRemovido = dados.videos.splice(indiceDoVideo, 1);
-      res.status(200).json({ mensagem: `Vídeo id:${id} removido com sucesso!`, videoRemovido });
-    } else {
-      res.status(404).json({ mensagem: "Vídeo não encontrado" });
-    }
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro ao remover vídeo", detalhes: erro.message });
-  }
-});
+router.delete("/:id", videosControllers.delete);
 
 module.exports = router;
